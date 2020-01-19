d1337337aceb7cf64a5b318be7038368ab456161
Marvin Arnold <marvin@geeky.rocks>
2020-01-19 16:34:22 -0600

    Echo messages to all clients other than sender

--- index.js
+++ index.js
@@ -7,11 +7,29 @@ const wss = new WebSocket.Server({ port: port });
 
 let numClients = 0
 
-const sendNumClients = () => {
-  console.log("Notifying clients about peers")
+// Is the client ready but not the same instance as ws (self)
+const isReadyClientNotSelf = (client, ws) => {
+  return (client !== ws) && (client.readyState === WebSocket.OPEN)
+}
+
+const sendNumClients = ws => {
+  console.log("Notifying self about number of peers")
+  ws.send("N:" + numClients)
+  // wss.clients.forEach(client => {
+  //   if (isReadyClientNotSelf(client, ws))
+  //     client.send("N:" + numClients)
+  // })
+}
+
+const echoMsg = (msg, ws) => {
+  console.log("Forwarding message to all clients except self")
   wss.clients.forEach(client => {
-    if (client !== wss && client.readyState === WebSocket.OPEN)
-      client.send(numClients)
+    if (isReadyClientNotSelf(client, ws)) {
+      console.log("sending msg to other")
+      client.send(msg)
+    } else {
+      console.log("not sending msg to self")
+    }
   })
 }
 
@@ -20,19 +38,22 @@ const sendNumClients = () => {
 */
 wss.on('connection', function connection(ws, req) {
   console.log("Got a connection over the websocket")
+
   numClients += 1
 
   console.log("New client connected. Num connected clients: " + numClients)
-  sendNumClients()
+  // This will trigger the client to create an offer
+  // Only support two peers right now. For simplicity, this will cause the newest client to create the initial offer.
+  sendNumClients(ws)
   
   ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
+    echoMsg(message, ws)
   })
 
   ws.on('close', function close() {
     console.log("A client disconnected")
     numClients -= 1
-    sendNumClients()
+    sendNumClients(ws)
   })
 
 });
